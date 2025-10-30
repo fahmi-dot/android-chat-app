6051a99fe4b1e172621b14e2168e8784bb3fef0b
Pahmiii <agilfahmirakasaputra073@gmail.com>
2025-10-30 17:16:59 +0700

    chore: add app_router.dart

---
+++ lib/core/router/app_router.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import '../../../features/auth/presentation/screens/splash_screen.dart';
+
+class Routes {
+  static const splash = '/';
+  static const login = '/login';
+  static const register = 'register';
+  static const chatList = '/chats';
+  static const chatRoom = '/chats/:roomId';
+}
+
+final appRouter = GoRouter(
+  initialLocation: Routes.splash,
+  routes: [
+    GoRoute(
+      path: Routes.splash,
+      builder: (context, state) => const SplashScreen(),
+    ),
+    GoRoute(
+      path: Routes.login,
+      // builder: (context, state) => const LoginScreen(),
+      builder: (context, state) => Scaffold(
+        body: Center(
+          child: Text("Login Page"),
+        ),
+      ),
+    ),
+    GoRoute(
+      path: Routes.chatList,
+      // builder: (context, state) => const ChatListScreen(),
+      builder: (context, state) => Scaffold(
+        body: Center(
+          child: Text("Chat List Page"),
+        ),
+      ),
+    ),
+    GoRoute(
+      path: Routes.chatRoom,
+      // builder: (context, state) {
+      //   final roomId = state.pathParameters['roomId']!;
+      //   return ChatRoomScreen(roomId: roomId);
+      // },
+      builder: (context, state) => Scaffold(
+        body: Center(
+          child: Text("Chat Room Page"),
+        ),
+      ),
+    ),
+  ],
+);
