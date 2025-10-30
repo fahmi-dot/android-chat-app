aaa35b52f056f3cfc82a0d2d7315986fdbb8a9d3
Pahmiii <agilfahmirakasaputra073@gmail.com>
2025-10-30 17:16:12 +0700

    chore: add constant files

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Light Theme Colors
+  static const Color primary = Color.fromRGBO(33, 150, 243, 1);
+  static const Color secondary = Color.fromRGBO(3, 169, 244, 1);
+  
+  static const Color background = Color.fromRGBO(245, 245, 245, 1);
+  static const Color surface = Color.fromRGBO(255, 255, 255, 1);
+
+  static const Color textPrimary = Color.fromRGBO(33, 33, 33, 1);
+  static const Color textSecondary = Color.fromRGBO(117, 117, 117, 1);
+
+  static const Color error = Color.fromRGBO(229, 57, 53, 1);
+  static const Color success = Color.fromRGBO(67, 160, 71, 1);
+  static const Color warning = Color.fromRGBO(251, 140, 0, 1);
+
+  // Dark Theme Colors
+  static const Color darkPrimary = Color.fromRGBO(51, 144, 236, 1);
+  static const Color darkSecondary = Color.fromRGBO(42, 139, 231, 1);
+
+  static const Color darkBackground = Color.fromRGBO(24, 27, 31, 1);
+  static const Color darkSurface = Color.fromRGBO(34, 38, 43, 1);
+
+  static const Color darkTextPrimary = Color.fromRGBO(236, 239, 244, 1);
+  static const Color darkTextSecondary = Color.fromRGBO(156, 160, 165, 1);
+
+  static const Color darkError = Color.fromRGBO(229, 115, 115, 1);
+  static const Color darkSuccess = Color.fromRGBO(102, 187, 106, 1);
+  static const Color darkWarning = Color.fromRGBO(255, 183, 77, 1);
+}
