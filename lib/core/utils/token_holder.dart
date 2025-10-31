86b4da276ea8e7f654d5ec58ecbb86fb59e1cdb9
Pahmiii <agilfahmirakasaputra073@gmail.com>
2025-10-31 14:27:20 +0700

    feat: update token_holder.dart

--- lib/core/utils/token_holder.dart
+++ lib/core/utils/token_holder.dart
@@ -2,17 +2,28 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class TokenHolder {
   static final _storage = FlutterSecureStorage();
-  static const _keyToken = 'TOKEN';
 
-  static Future<void> saveToken(String token) async {
-    await _storage.write(key: _keyToken, value: token);
+  static const _keyAccessToken = 'ACCESS_TOKEN';
+  static const _keyRefreshToken = 'REFRESH_TOKEN';
+
+  static Future<void> saveTokens({
+    required String accessToken,
+    required String refreshToken,
+  }) async {
+    await _storage.write(key: _keyAccessToken, value: accessToken);
+    await _storage.write(key: _keyRefreshToken, value: refreshToken);
   }
 
-  static Future<String?> getToken() async {
-    return await _storage.read(key: _keyToken);
+  static Future<String?> getAccessToken() async {
+    return await _storage.read(key: _keyAccessToken);
   }
 
-  static Future<void> deleteToken() async {
-    await _storage.delete(key: _keyToken);
+  static Future<String?> getRefreshToken() async {
+    return await _storage.read(key: _keyRefreshToken);
+  }
+
+  static Future<void> deleteTokens() async {
+    await _storage.delete(key: _keyAccessToken);
+    await _storage.delete(key: _keyRefreshToken);
   }
 }
