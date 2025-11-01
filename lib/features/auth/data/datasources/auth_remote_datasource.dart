d0ba8986e08f1d25d745da5eea5d89831eec9e1d
Pahmiii <agilfahmirakasaputra073@gmail.com>
2025-11-01 12:36:10 +0700

    feat: add check and refresh method for managing token

--- lib/features/auth/data/datasources/auth_remote_datasource.dart
+++ lib/features/auth/data/datasources/auth_remote_datasource.dart
@@ -2,9 +2,12 @@ import 'package:android_chat_app/core/network/api_client.dart';
 import 'package:android_chat_app/core/utils/token_holder.dart';
 import 'package:android_chat_app/features/auth/data/models/auth_model.dart';
 import 'package:android_chat_app/features/auth/domain/entities/auth.dart';
+import 'package:dio/dio.dart';
 
 abstract class AuthRemoteDataSource {
   Future<Auth> login(String username, String password);
+  Future<Auth> check();
+  Future<Auth> refresh();
 }
 
 class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
@@ -16,7 +19,7 @@ class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
   Future<Auth> login(String username, String password) async {
     final response = await api.post(
       '/auth/login',
-      data: {'username': username, 'password': password},
+      data: {'username': username, 'password': password}
     );
 
     final data = response.data['data'];
@@ -26,6 +29,40 @@ class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
       refreshToken: data['tokens']['refreshToken']
     );
 
-    return AuthModel(isLoggedIn: true).toEntity();
+    return AuthModel(isLoggedIn: data != null).toEntity();
+  }
+  
+  @override
+  Future<Auth> check() async {
+    try {
+      final response = await api.get('/user');
+
+      final data = response.data['data'];
+
+      return (data != null)
+        ? AuthModel(isLoggedIn: data != null).toEntity()
+        : refresh();
+    } on DioException {
+      return refresh();
+    }
+  }
+
+  @override
+  Future<Auth> refresh() async {
+    final refreshToken = await TokenHolder.getRefreshToken();
+
+    final response = await api.post(
+      '/auth/refresh',
+      data: {'refreshToken': refreshToken}
+    );
+
+    final data = response.data['data'];
+
+    await TokenHolder.saveTokens(
+      accessToken: data['accessToken'],
+      refreshToken: data['refreshToken']
+    );
+
+    return AuthModel(isLoggedIn: data != null).toEntity();
   }
 }
