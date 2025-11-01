c39140b5ff306bba74579cbc2583e188202a912e
Pahmiii <agilfahmirakasaputra073@gmail.com>
2025-11-01 12:46:29 +0700

    feat: add navigation logic to redirect based on auth state

--- lib/features/auth/presentation/screens/splash_screen.dart
+++ lib/features/auth/presentation/screens/splash_screen.dart
@@ -1,24 +1,35 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:android_chat_app/features/auth/presentation/providers/auth_provider.dart';
 
-class SplashScreen extends StatefulWidget {
+class SplashScreen extends ConsumerStatefulWidget {
   const SplashScreen({super.key});
 
   @override
-  State<SplashScreen> createState() => _SplashScreenState();
+  ConsumerState<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends ConsumerState<SplashScreen> {
+
   @override
   void initState() {
     super.initState();
+    _navigateAfterDelay();
+  }
 
-    Timer(const Duration(seconds: 2), () {
-      if (mounted) {
-        context.go('/login');
-      }
-    });
+  Future<void> _navigateAfterDelay() async {
+    await Future.delayed(const Duration(seconds: 2));
+
+    if (!context.mounted) return;
+
+    final authState = await ref.read(authProvider.future);
+    if (authState != null) {
+      context.go('/chats');
+    } else {
+      context.go('/login');
+    }
   }
 
   @override
