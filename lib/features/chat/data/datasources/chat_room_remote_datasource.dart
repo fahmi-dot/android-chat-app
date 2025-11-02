45b92fe8878fa2760375f9eb9cfdf1d1529d3f5f
Pahmiii <agilfahmirakasaputra073@gmail.com>
2025-11-02 16:36:51 +0700

    feat: add chat chat room remote datasource

---
+++ lib/features/chat/data/datasources/chat_room_remote_datasource.dart
@@ -0,0 +1,28 @@
+import 'package:android_chat_app/core/network/api_client.dart';
+import 'package:android_chat_app/features/chat/data/models/chat_room_model.dart';
+import 'package:android_chat_app/features/chat/domain/entities/chat_room.dart';
+
+abstract class ChatRoomRemoteDataSource {
+  Future<List<ChatRoom>> getMessages(String roomId);
+}
+
+class ChatRoomRemoteDataSourceImpl extends ChatRoomRemoteDataSource {
+  final ApiClient api;
+
+  ChatRoomRemoteDataSourceImpl({required this.api});
+
+  @override
+  Future<List<ChatRoom>> getMessages(String roomId) async {
+    final response = await api.get('/chat/$roomId/messages');
+    final data = response.data['data'] as List;
+
+    return data.map((message) {
+      return ChatRoomModel(
+        id: message['id'],
+        content: message['content'],
+        sentAt: message['sentAt'],
+        senderId: message['senderId'],
+      ).toEntity();
+    }).toList();
+  }
+}
