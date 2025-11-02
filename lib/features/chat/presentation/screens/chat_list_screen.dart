2aaadd10f0d4597d64a7d04ee6f4d592e6a27863
Pahmiii <agilfahmirakasaputra073@gmail.com>
2025-11-02 16:33:04 +0700

    feat: update navigate to use .push

--- lib/features/chat/presentation/screens/chat_list_screen.dart
+++ lib/features/chat/presentation/screens/chat_list_screen.dart
@@ -20,23 +20,23 @@ class _ChatListScreenState extends ConsumerState<ChatListScreen> {
       body: chatListState.when(
         loading: () => const Center(child: CircularProgressIndicator()),
         error: (e, _) => Center(child: Text('Error loading chats: $e')),
-        data: (chatList) {
-          if (chatList == null || chatList.isEmpty) {
+        data: (room) {
+          if (room == null || room.isEmpty) {
             return const Center(child: Text('No chats found'));
           }
 
           return ListView.builder(
-            itemCount: chatList.length,
+            itemCount: room.length,
             itemBuilder: (context, index) {
-              final chat = chatList[index];
+              final r = room[index];
               return ListTile(
                 leading: CircleAvatar(
-                  backgroundImage: NetworkImage(chat.avatarUrl),
+                  backgroundImage: NetworkImage(r.avatarUrl),
                 ),
-                title: Text(chat.displayName),
-                subtitle: Text(chat.username),
+                title: Text(r.displayName),
+                subtitle: Text(r.username),
                 onTap: () {
-                  context.go('/chats/$chat.id');
+                  context.push('/chats/${r.id}');
                 },
               );
             },
