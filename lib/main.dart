4656b95a3f403b4bb0d3a587b6a77c68c5bb17ff
Pahmiii <agilfahmirakasaputra073@gmail.com>
2025-11-04 20:16:13 +0700

    feat: add initializer WebSocket client to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:android_chat_app/core/network/ws_client.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await WsClient().initialize();
   runApp(
     const ProviderScope(
       child: MyApp(),
