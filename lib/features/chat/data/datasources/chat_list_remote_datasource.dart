9663f798638092ddc7c5395bc1a0d33c6a6c8e57
Pahmiii <agilfahmirakasaputra073@gmail.com>
2025-11-04 20:18:04 +0700

    feat: implement new attribute and add try catch

--- lib/features/chat/data/datasources/chat_list_remote_datasource.dart
+++ lib/features/chat/data/datasources/chat_list_remote_datasource.dart
@@ -1,6 +1,7 @@
 import 'package:android_chat_app/core/network/api_client.dart';
 import 'package:android_chat_app/features/chat/data/models/chat_list_model.dart';
 import 'package:android_chat_app/features/chat/domain/entities/chat_list.dart';
+import 'package:dio/dio.dart';
 
 abstract class ChatListRemoteDataSource {
   Future<List<ChatList>> getRooms();
@@ -10,26 +11,38 @@ class ChatListRemoteDataSourceImpl implements ChatListRemoteDataSource {
   final ApiClient api;
   final String currentUsername;
 
-  ChatListRemoteDataSourceImpl({required this.api, required this.currentUsername});
+  ChatListRemoteDataSourceImpl({
+    required this.api,
+    required this.currentUsername,
+  });
 
   @override
   Future<List<ChatList>> getRooms() async {
-    final response = await api.get('/chat/list');
-    final data = response.data['data'] as List;
+    try {
+      final response = await api.get('/chat/list');
+      final data = response.data['data'] as List;
 
-    return data.map((room) {
-      final participants = room['participants'] as List;
-      final target = participants.firstWhere(
-        (p) => p['username'] != currentUsername,
-        orElse: () => null,
-      );
+      return data.map((room) {
+        final participants = room['participants'] as List;
+        final target = participants.firstWhere(
+          (p) => p['username'] != currentUsername,
+          orElse: () => null,
+        );
 
-      return ChatListModel(
-        id: room['id'],
-        username: target?['username'] ?? '',
-        displayName: target?['displayName'] ?? '',
-        avatarUrl: target?['avatarUrl'] ?? '',
-      ).toEntity();
-    }).toList();
+        return ChatListModel(
+          id: room['id'],
+          username: target?['username'] ?? '',
+          displayName: target?['displayName'] ?? '',
+          avatarUrl: target?['avatarUrl'] ?? '',
+          lastMessage: room['lastMessage'] ?? '',
+          lastMessageSentAt: room['lastMessageSentAt'] != null
+              ? DateTime.parse(room['lastMessageSentAt'])
+              : DateTime.now(),
+          unreadMessagesCount: room['unreadMessagesCount'],
+        ).toEntity();
+      }).toList();
+    } on DioException catch (e) {
+      throw Exception('Gagal mendapatkan rooms: $e');
+    }
   }
 }
