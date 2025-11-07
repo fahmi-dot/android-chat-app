4cfe9367a4a84963675d9d9e69627a42be4ddd9b
Pahmiii <agilfahmirakasaputra073@gmail.com>
2025-11-07 20:56:39 +0700

    feat: update chat list screen UI

--- lib/features/chat/presentation/screens/chat_list_screen.dart
+++ lib/features/chat/presentation/screens/chat_list_screen.dart
@@ -1,3 +1,5 @@
+import 'package:android_chat_app/core/constants/app_colors.dart';
+import 'package:android_chat_app/features/auth/presentation/providers/auth_provider.dart';
 import 'package:android_chat_app/features/chat/presentation/providers/chat_list_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -11,12 +13,34 @@ class ChatListScreen extends ConsumerStatefulWidget {
 }
 
 class _ChatListScreenState extends ConsumerState<ChatListScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    ref.read(chatListProvider);
+  }
+
+  void _logout() async {
+    await ref.read(authProvider.notifier).logout();
+
+    context.go('/');
+  }
+
   @override
   Widget build(BuildContext context) {
     final chatListState = ref.watch(chatListProvider);
 
     return Scaffold(
-      appBar: AppBar(title: const Text('Chats')),
+      appBar: AppBar(
+        title: const Text('Chats'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.power_settings_new_rounded),
+            onPressed: _logout,
+          ),
+        ],
+      ),
       body: chatListState.when(
         loading: () => const Center(child: CircularProgressIndicator()),
         error: (e, _) => Center(child: Text('Error loading chats: $e')),
@@ -33,8 +57,35 @@ class _ChatListScreenState extends ConsumerState<ChatListScreen> {
                 leading: CircleAvatar(
                   backgroundImage: NetworkImage(r.avatarUrl),
                 ),
-                title: Text(r.displayName),
-                subtitle: Text(r.username),
+                title: Text(
+                  r.displayName,
+                  style: const TextStyle(fontWeight: FontWeight.w600),
+                ),
+                subtitle: Text(
+                  r.lastMessage,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: const TextStyle(color: Colors.grey),
+                ),
+                trailing: r.unreadMessagesCount > 0
+                    ? Container(
+                        width: 24,
+                        height: 24,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          color: AppColors.primary,
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: Text(
+                          r.unreadMessagesCount.toString(),
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 12,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      )
+                    : null,
                 onTap: () {
                   context.push('/chats/${r.id}');
                 },
