345eb1f3896e94da23bcd236b15c34ba25161ce1
Pahmiii <agilfahmirakasaputra073@gmail.com>
2025-11-11 22:13:16 +0700

    chore: add new constant variables in app sizes

--- lib/core/constants/app_sizes.dart
+++ lib/core/constants/app_sizes.dart
@@ -3,17 +3,21 @@ class AppSizes {
   static const double fontS = 12.0;
   static const double fontM = 14.0;
   static const double fontL = 16.0;
-  static const double fontXL = 20.0;
+  static const double fontXL = 18.0;
+  static const double fontXXL = 20.0;
 
   static const double iconS = 20.0;
   static const double iconM = 24.0;
-  static const double iconL = 32.0;
+  static const double iconL = 28.0;
+  static const double iconXL = 32.0;
+  static const double iconXXL = 80.0;
   
   static const double paddingXS = 4.0;
   static const double paddingS = 8.0;
   static const double paddingM = 16.0;
-  static const double paddingL = 24.0;
-  static const double paddingXL = 32.0;
+  static const double paddingL = 18.0;
+  static const double paddingXL = 24.0;
+  static const double paddingXXL = 32.0;
   
   static const double radiusXS = 4.0;
   static const double radiusS = 8.0;
