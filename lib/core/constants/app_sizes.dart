6a3342150f9aafd96f1017439b53277841c35722
Pahmiii <agilfahmirakasaputra073@gmail.com>
2025-11-12 13:09:41 +0700

    chore: update constant files

--- lib/core/constants/app_sizes.dart
+++ lib/core/constants/app_sizes.dart
@@ -1,4 +1,9 @@
+import 'package:flutter/material.dart';
+
 class AppSizes {
+  static double screenWidth(BuildContext context) => MediaQuery.of(context).size.width;
+  static double screenHeight(BuildContext context) => MediaQuery.of(context).size.height;
+
   static const double fontXS = 9.0;
   static const double fontS = 12.0;
   static const double fontM = 14.0;
@@ -6,22 +11,24 @@ class AppSizes {
   static const double fontXL = 18.0;
   static const double fontXXL = 20.0;
 
+  static const double iconXS = 16.0;
   static const double iconS = 20.0;
   static const double iconM = 24.0;
   static const double iconL = 28.0;
   static const double iconXL = 32.0;
-  static const double iconXXL = 80.0;
+  static const double iconXXL = 36.0;
   
   static const double paddingXS = 4.0;
   static const double paddingS = 8.0;
   static const double paddingM = 16.0;
-  static const double paddingL = 18.0;
+  static const double paddingL = 20.0;
   static const double paddingXL = 24.0;
-  static const double paddingXXL = 32.0;
+  static const double paddingXXL = 28.0;
   
   static const double radiusXS = 4.0;
   static const double radiusS = 8.0;
   static const double radiusM = 12.0;
   static const double radiusL = 16.0;
   static const double radiusXL = 20.0;
+  static const double radiusXXL = 28.0;
 }
--- lib/core/constants/app_strings.dart
+++ lib/core/constants/app_strings.dart
@@ -1,3 +1,15 @@
 class AppStrings {
-  static const String appName = "MiConnect";  
+  static const String appName = 'Hello!';
+
+  static const String login = 'Login';
+  static const String loginTitle = 'Login to your account';
+  static const String register = 'Register';
+  static const String registerTitle = 'Create an account';
+
+  static const String phoneNumber = 'Phone number';
+  static const String username = 'Username';
+  static const String password = 'Password';
+
+  static const String forgot = 'Forgot password?';
+  static const String noAccount = "Don't have an account?";
 }
