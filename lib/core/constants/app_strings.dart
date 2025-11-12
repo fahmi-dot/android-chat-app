9807a5ea4b7f00777d1a031a0649ec7047079adb
Pahmiii <agilfahmirakasaputra073@gmail.com>
2025-11-12 21:56:55 +0700

    chore: update app strings file

--- lib/core/constants/app_strings.dart
+++ lib/core/constants/app_strings.dart
@@ -5,11 +5,17 @@ class AppStrings {
   static const String loginTitle = 'Login to your account';
   static const String register = 'Register';
   static const String registerTitle = 'Create an account';
+  static const String forgot = 'Forgot password?';
+  static const String noAccount = "Don't have an account?";
 
   static const String phoneNumber = 'Phone number';
   static const String username = 'Username';
   static const String password = 'Password';
+  static const String message = 'Message';
+  static const String search = 'Search';
 
-  static const String forgot = 'Forgot password?';
-  static const String noAccount = "Don't have an account?";
+  static const String noChats = 'No chats yet';
+  static const String noChatsSub = 'Start a new conversation';
+  static const String noChatsFound = 'No chats found';
+  static const String noMessages = 'No messages yet';
 }
