1aeaed39404c0d33c05fe89e65c20663053e7f61
Pahmiii <agilfahmirakasaputra073@gmail.com>
2025-11-20 16:56:32 +0700

    feat: add theme provider and implement to MyApp

--- lib/app.dart
+++ lib/app.dart
@@ -1,19 +1,21 @@
+import 'package:android_chat_app/core/theme/theme_provider.dart';
 import 'package:flutter/material.dart';
-import 'package:android_chat_app/core/constants/app_strings.dart';
 import 'package:android_chat_app/core/router/app_router.dart';
 import 'package:android_chat_app/core/theme/app_theme.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final themeState = ref.watch(themeProvider);
+
     return MaterialApp.router(
-      title: AppStrings.appName,
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
-      themeMode: ThemeMode.light,
+      themeMode: themeState,
       routerConfig: appRouter,
     );
   }
---
+++ lib/core/theme/theme_provider.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+final themeProvider = NotifierProvider<ThemeNotifier, ThemeMode>(
+  ThemeNotifier.new,
+);
+
+class ThemeNotifier extends Notifier<ThemeMode> {
+  static const _key = 'app_theme_mode';
+
+  @override
+  ThemeMode build() {
+    _loadTheme();
+    return ThemeMode.system;
+  }
+
+  Future<void> _loadTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    final value = prefs.getString(_key);
+
+    if (value == 'light') {
+      state = ThemeMode.light;
+    } else if (value == 'dark') {
+      state = ThemeMode.dark;
+    } else {
+      state = ThemeMode.system;
+    }
+  }
+
+  Future<void> _saveTheme(ThemeMode mode) async {
+    final prefs = await SharedPreferences.getInstance();
+
+    switch (mode) {
+      case ThemeMode.light:
+        await prefs.setString(_key, 'light');
+        break;
+      case ThemeMode.dark:
+        await prefs.setString(_key, 'dark');
+        break;
+      case ThemeMode.system:
+        await prefs.setString(_key, 'system');
+        break;
+    }
+  }
+
+  void toggle() {
+    if (state != ThemeMode.dark) {
+      setTheme(ThemeMode.dark);
+    } else {
+      setTheme(ThemeMode.light);
+    }
+  }
+
+  Future<void> setTheme(ThemeMode mode) async {
+    state = mode;
+    await _saveTheme(mode);
+  }
+}
