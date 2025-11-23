4e097a76022f08d38dec5670e531053653ba9efc
Pahmiii <agilfahmirakasaputra073@gmail.com>
2025-11-23 19:57:09 +0700

    style: update color variables in AppColors

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -3,22 +3,22 @@ import 'package:flutter/material.dart';
 class AppColors {
   // Main
   static const Color primary = Color.fromRGBO(74, 144, 226, 1);
-  static const Color secondary = Color.fromRGBO(26, 115, 232, 1);
+  static const Color secondary = Color.fromRGBO(76, 165, 226, 1);
 
   // Light
-  static const Color background = Color.fromRGBO(245, 245, 245, 1);
-  static const Color surface = Color.fromRGBO(235, 235, 235, 1);
-  static const Color textPrimary = Color.fromRGBO(0, 0, 0, 1);
-  static const Color textSecondary = Color.fromRGBO(165, 165, 165, 1);
+  static const Color background = Color.fromRGBO(242, 250, 255, 1);
+  static const Color surface = Color.fromRGBO(222, 230, 255, 1);
+  static const Color textPrimary = Color.fromRGBO(0, 25, 38, 1);
+  static const Color textSecondary = Color.fromRGBO(30, 55, 68, 1);
 
   // Dark
-  static const Color darkBackground = Color.fromRGBO(18, 18, 18, 1);
-  static const Color darkSurface = Color.fromRGBO(51, 51, 51, 1);
-  static const Color darkTextPrimary = Color.fromRGBO(230, 230, 230, 1);
-  static const Color darkTextSecondary = Color.fromRGBO(175, 175, 175, 1);
+  static const Color darkBackground = Color.fromRGBO(0, 25, 38, 1);
+  static const Color darkSurface = Color.fromRGBO(30, 55, 68, 1);
+  static const Color darkTextPrimary = Color.fromRGBO(242, 250, 255, 1);
+  static const Color darkTextSecondary = Color.fromRGBO(222, 230, 255, 1);
 
   // Additional
-  static const Color error = Color.fromRGBO(229, 115, 115, 1);
+  static const Color error = Color.fromRGBO(156, 33, 33, 1);
   static const Color success = Color.fromRGBO(102, 187, 106, 1);
   static const Color warning = Color.fromRGBO(255, 183, 77, 1);
 }
