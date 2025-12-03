b1a9019ae8df65b5faebee100c511a05fe70f170
Pahmiii <agilfahmirakasaputra073@gmail.com>
2025-12-03 19:59:02 +0700

    feat: integrate local datasource and add pre-fetch local check before retrieving remote chat rooms

--- lib/features/chat/data/repositories/chat_list_repository_impl.dart
+++ lib/features/chat/data/repositories/chat_list_repository_impl.dart
@@ -1,18 +1,42 @@
-import 'package:android_chat_app/features/chat/data/datasources/chat_list_remote_datasource.dart';
+import 'dart:async';
+
+import 'package:android_chat_app/features/chat/data/datasources/local/chat_list_local_datasource.dart';
+import 'package:android_chat_app/features/chat/data/datasources/remote/chat_list_remote_datasource.dart';
+import 'package:android_chat_app/features/chat/data/models/local/room_local_model.dart';
 import 'package:android_chat_app/features/chat/domain/entities/room.dart';
 import 'package:android_chat_app/features/chat/domain/repositories/chat_list_repository.dart';
 
 class ChatListRepositoryImpl implements ChatListRepository {
   final ChatListRemoteDataSource chatListRemoteDataSource;
+  final ChatListLocalDataSource chatListLocalDataSource;
 
   ChatListRepositoryImpl({
     required this.chatListRemoteDataSource,
+    required this.chatListLocalDataSource,
   });
 
   @override
   Future<List<Room>> getChatRooms() async {
-    final roomModels = await chatListRemoteDataSource.getChatRooms();
+    final roomRemoteModels = await chatListRemoteDataSource.getChatRooms();
 
-    return roomModels.map((room) => room.toEntity()).toList();
+    if (roomRemoteModels.isNotEmpty) {
+      unawaited(_getFromRemote());
+
+      return roomRemoteModels.map((room) => room.toEntity()).toList();
+    }
+
+    return await _getFromRemote();
+  }
+
+  Future<List<Room>> _getFromRemote() async {
+    final roomRemoteModels = await chatListRemoteDataSource.getChatRooms();
+    
+    for (final remote in roomRemoteModels) {
+      final roomLocalModel = RoomLocalModel.fromEntity(remote.toEntity());
+
+      await chatListLocalDataSource.upsertChatRoom(roomLocalModel);
+    }
+
+    return roomRemoteModels.map((room) => room.toEntity()).toList();
   }
 }
