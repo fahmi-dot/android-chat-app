9cc8fb573eff5fbb041eeda44a3085428010656e
Pahmiii <agilfahmirakasaputra073@gmail.com>
2025-12-06 17:10:30 +0700

    feat: add expiryAt attribute in token entity and model file

--- lib/features/auth/data/models/token_model.dart
+++ lib/features/auth/data/models/token_model.dart
@@ -4,19 +4,22 @@ class TokenModel extends Token {
   const TokenModel({
     required super.access,
     required super.refresh,
+    required super.expiryAt,
   });
 
   factory TokenModel.fromEntity(Token token) {
     return TokenModel(
       access: token.access,
       refresh: token.refresh,
+      expiryAt: token.expiryAt,
     );
   }
 
   factory TokenModel.fromJson(Map<String, dynamic> json) {
     return TokenModel(
       access: json['accessToken'], 
-      refresh: json['refreshToken']
+      refresh: json['refreshToken'],
+      expiryAt: DateTime.parse(json['expiryAt']),
     );
   }
 
@@ -24,6 +27,7 @@ class TokenModel extends Token {
     return Token(
       access: access,
       refresh: refresh,
+      expiryAt: expiryAt, 
     );
   }
 
--- lib/features/auth/domain/entities/token.dart
+++ lib/features/auth/domain/entities/token.dart
@@ -3,15 +3,18 @@ import 'package:equatable/equatable.dart';
 class Token extends Equatable {
   final String access;
   final String refresh;
+  final DateTime expiryAt;
 
   const Token({
     required this.access,
     required this.refresh,
+    required this.expiryAt,
   });
 
   @override
   List<Object?> get props => [
     access,
     refresh,
+    expiryAt,
   ];
 }
